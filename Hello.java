94529106869cd7584d3337e61a883a666c292f24
Behar1231 <112477255+Behar1231@users.noreply.github.com>
2022-09-05 11:59:24 +0200

    Hello

---
+++ Hello.java
@@ -0,0 +1,11 @@
+public class Hello
+{
+    public static void main(String[] args)
+    {
+        String helloWorld[] = {"H", "e", "l", "l", "o"};
+        for(int i = 0; i < helloWorld.length; i++)  {
+            String str = hwlloWorld[i];
+            System.out.printIn(str);
+        
+    }
+    } 
